081d6730b8833b9282ea953e5c4f8e00e745c151
Mike Konczal <100174099+mtkonczal@users.noreply.github.com>
2025-04-16 00:05:02 -0400

    Diffusion index.

---
+++ R/data-cesDiffusionIndex.R
@@ -0,0 +1,23 @@
+#' cesDiffusionIndex Dataset
+#'
+#' A tibble with 250 rows and 2 columns representing industry codes and corresponding industry titles.
+#'
+#' This dataset contains information on different industries, where each row corresponds to an industry
+#' defined by its unique code and a descriptive title. It is useful for analyses that require linking industry
+#' classifications to descriptive labels.
+#'
+#' @format A tibble with 250 rows and 2 variables:
+#' \describe{
+#'   \item{ces_industry_code}{A character vector containing the industry codes (e.g., "10-11330000").}
+#'   \item{ces_industry_title}{A character vector containing the titles of the industries (e.g., "Logging").}
+#' }
+#'
+#' @source [Data incoming on this]
+#'
+#' @examples
+#' # Load the dataset
+#' data(cesDiffusionIndex)
+#'
+#' # Display the first few rows
+#' head(cesDiffusionIndex)
+"cesDiffusionIndex"
